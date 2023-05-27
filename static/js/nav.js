63df682a819b7b2fa456709199156eeb554e542a
Stefan Atanaskovic <stefan.atanaskovic02@gmail.com>
2023-05-28 01:08:09 +0200

    deleted cv file + maybe optimized nav + removed twitter link

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,10 +1,12 @@
 let curNav = "aboutNav";
+let currentSelcetedNavElement = document.getElementById("aboutNav");
 
 function setSelected(clickedId) {
     if (curNav === clickedId) {
         return
     }
-    document.getElementById(curNav).classList.remove("selected");
-    document.getElementById(clickedId).classList.add("selected");
+    currentSelcetedNavElement.classList.remove("selected");
+    currentSelcetedNavElement = document.getElementById(clickedId);
+    currentSelcetedNavElement.classList.add("selected");
     curNav = clickedId;
 }
